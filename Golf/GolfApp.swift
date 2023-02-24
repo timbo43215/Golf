ecfbbbcf8985ff9f5d5c6eda2824014f5a15fd04
Tim Stack <tstack43215@yahoo.com>
2023-02-24 15:06:50 -0600

    Initial Commit

---
+++ Golf/GolfApp.swift
@@ -0,0 +1,17 @@
+//
+//  GolfApp.swift
+//  Golf
+//
+//  Created by IIT PHYS 440 on 2/24/23.
+//
+
+import SwiftUI
+
+@main
+struct GolfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
