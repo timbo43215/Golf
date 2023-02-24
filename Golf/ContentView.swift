e1588e3e8b09e63be9fe11d8b2e4548e389d8bd2
Tim Stack <tstack43215@yahoo.com>
2023-02-24 15:16:24 -0600

    ajsdfhl

--- Golf/ContentView.swift
+++ Golf/ContentView.swift
@@ -7,20 +7,76 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct GolfChippingView: View {
+    
+    @State private var ballPosition: CGPoint = CGPoint(x: 100, y: 500)
+    @State private var holePosition: CGPoint = CGPoint(x: 300, y: 300)
+    @State private var isHoleReached = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        GeometryReader { geometry in
+            let greenRect = CGRect(x: 0, y: 0, width: geometry.size.width, height: geometry.size.height)
+            VStack {
+                Canvas { context, size in
+                    // Draw the green
+                    context.stroke(Path(roundedRect: greenRect, cornerRadius: 10), with: .color(Color.green), lineWidth: 10)
+                    
+                    // Draw the hole
+                    context.fill(Path(ellipseIn: CGRect(x: holePosition.x - 10, y: holePosition.y - 10, width: 20, height: 20)), with: .color(Color.black))
+                    
+                    // Draw the ball
+                    context.fill(Path(ellipseIn: CGRect(x: ballPosition.x - 10, y: ballPosition.y - 10, width: 20, height: 20)), with: .color(Color.blue))
+                }
+                .frame(width: geometry.size.width, height: geometry.size.height)
+                .gesture(
+                    DragGesture()
+                        .onChanged { value in
+                            let newBallPosition = CGPoint(x: value.location.x, y: value.location.y)
+                            if greenRect.contains(newBallPosition) {
+                                ballPosition = newBallPosition
+                            }
+                        }
+                        .onEnded { _ in
+                            if !isHoleReached {
+                                let distance = ballPosition.distance(to: holePosition)
+                                if distance <= 30 {
+                                    isHoleReached = true
+                                    ballPosition = holePosition
+                                } else {
+                                    let angle = Double.random(in: 0..<Double.pi * 2)
+                                    let dx = cos(angle) * distance / 10
+                                    let dy = sin(angle) * distance / 10
+                                    let newBallPosition = CGPoint(x: ballPosition.x + CGFloat(dx), y: ballPosition.y + CGFloat(dy))
+                                    ballPosition = newBallPosition
+                                }
+                            }
+                        }
+                )
+                
+                if isHoleReached {
+                    Text("You made it!")
+                        .font(.largeTitle)
+                        .foregroundColor(.green)
+                        .padding()
+                        .background(Color.green)
+                        .cornerRadius(10)
+                        .padding()
+                }
+            }
         }
-        .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+extension CGPoint {
+    func distance(to point: CGPoint) -> Double {
+        let dx = Double(point.x - self.x)
+        let dy = Double(point.y - self.y)
+        return sqrt(dx * dx + dy * dy)
+    }
+}
+
+struct GolfChippingView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        GolfChippingView()
     }
 }
--- Golf/GolfApp.swift
+++ Golf/GolfApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct GolfApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            GolfChippingView()
         }
     }
 }
